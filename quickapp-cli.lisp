dd1b04da1d4b073b79b803ec2b7ad3b7a7181a6c
Bryan Hoyle <starfoxprime@gmail.com>
2015-07-04 11:35:42 -0400

    More argument parsing stuff.

--- quickapp-cli.lisp
+++ quickapp-cli.lisp
@@ -59,19 +59,78 @@ appropriately"
 	(let* ((x (car arg-list)) ;; let x be the first argument
 		   (l (length x))) ;; let l be the next argument
 	  (cond
-		((and (>= l 2) (string= "--" (subseq x 0 2)))
-		 (multiple-value-bind (fst snd) (split-string-first "=" x)
-		   (list (list unnamed (cons (cons (subseq fst 2) snd) named)) (cdr arg-list))))
-		((string= "-" (subseq x 0 1))
-		 (list (list unnamed (cons (cons (subseq x 1) (cadr arg-list)) named)) (cddr arg-list)))
-		(t
+		((and (>= l 2) (string= "--" (subseq x 0 2))) ;; If we have a fully-named arg
+		 (multiple-value-bind (fst snd) (split-string-first "=" x) ;; split the arg on "="
+		   (list (list unnamed (cons (cons (subseq fst 2) snd) named)) ;;add to the named list
+				 (cdr arg-list)))) 
+		((string= "-" (subseq x 0 1)) ;; if we have an abbreviated arg
+		 (cond
+		   ((= l 2)
+			(list (list unnamed (cons (cons (subseq x 1) (cadr arg-list)) named))
+				  (cddr arg-list)))
+		   ((= l 1)
+			(list (list (cons "-" unnamed) named)
+				  (cdr arg-list)))
+		   (t
+			(list (list unnamed (cons (cons (subseq-rel x 1 1) (subseq x 2)) named)) ;;add to the named list
+				  (cdr arg-list))))) 
+		(t ;;otherwise, this is an unnamed argument
 		 (list (list (cons x unnamed) named) (cdr arg-list)))))))
 
 (defun parse-args (args)
-  (reduce-full #'parse-arg (list nil nil) args))
+  "Parse an argument list into the appropriate list"
+  (destructuring-bind (unnamed named) (reduce-full #'parse-arg (list nil nil) args)
+	(list (reverse unnamed) named)))
+
+(defun fix-named-arg (arg-names arg)
+  (let ((x (assoc (first arg) arg-names :test #'string=)))
+	(if x
+		(cons (second x) (cdr arg))
+		arg)))
+
+(defun fix-named-args (arg-names args)
+  (let ((named-args (cadr args)))
+	(list (first args)
+		  (mapcar (lambda (arg) (fix-named-arg arg-names arg)) named-args))))
+
+(defun fill-string (str fill-char n)
+  (with-string-stream st
+	(write-string str st)
+	(loop-rec ((n (- n (length str))))
+	   (unless (<= n 0)
+		 (write-char fill-char st)
+		 (rec (1- n))))))
+
+(defun fix-argdef (argdef)
+  (if (> (length argdef) 3)
+	  (list (if (> (length (first argdef)) 0)
+				(concatenate 'string "-"  (first argdef))
+				"")
+			(concatenate 'string "--" (second argdef) "=" (third argdef))
+			(car (last argdef)))
+	  (list (if (> (length (first argdef)) 0)
+				(concatenate 'string "-" (first argdef))
+				"")
+			(concatenate 'string "--" (second argdef))
+			(car (last argdef)))))
+
+(defun generate-help-string (arg-defs)
+  (let ((l (+ 2 (loop for x in arg-defs
+				   maximizing (length (second (fix-argdef x))) into l
+				   finally (return l)))))
+	(format nil "~{~a~%~}"
+			(mapcar (lambda (argdef)
+					  (apply #'concatenate 'string
+							 (list (fill-string (first argdef) #\Space 4)
+								   (fill-string (second argdef) #\Space l)
+								   (car (last argdef)))))
+					(mapcar #'fix-argdef arg-defs)))))
 
 (defun -main (&optional args)
-  (format t "~a~%" "I don't do much yet"))
+  "Entry point"
+  (let* ((parsed-args (parse-args (rest args)))
+		 (fixed-args (fix-named-args '(("f" "file")) parsed-args)))
+	(format t "~a~%" fixed-args)))
 
 (defun disable-debugger ()
   (labels
